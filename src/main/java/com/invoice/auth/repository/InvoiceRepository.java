d47ba3f336ca0c954e364e517309909ee5477b5b
jaiganesh707 <jaiganeshjkm@gmail.com>
2026-03-05 06:58:48 +0530

    InvoiceRepositoryaddedEntityGraph

--- src/main/java/com/invoice/auth/repository/InvoiceRepository.java
+++ src/main/java/com/invoice/auth/repository/InvoiceRepository.java
@@ -2,6 +2,7 @@ package com.invoice.auth.repository;
 
 import com.invoice.auth.entity.Invoice;
 import com.invoice.auth.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +10,9 @@ import java.util.List;
 
 @Repository
 public interface InvoiceRepository extends CrudRepository<Invoice, Integer> {
+    @EntityGraph(attributePaths = { "items" })
     List<Invoice> findAllByUserOrderByCreatedAtDesc(User user);
 
+    @EntityGraph(attributePaths = { "items" })
     List<Invoice> findAllByOrderByCreatedAtDesc();
 }
